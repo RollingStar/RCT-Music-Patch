3b022b85373946d330a40c068b03f10378ccbb36
Pau Ruiz i Safont <unduthegun@gmail.com>
2017-02-18 18:12:14 +0000

    Use FLAC command-line programs exclusively
    Now instead of patching the resulting files from the conversion
    we use files containing the metadata, extracted from previously-patched
    flacs.

--- RCT2 Sound Conversion.bat
+++ RCT2 Sound Conversion.bat
@@ -1,17 +1,14 @@
-::Convert WAVs (saved as "DAT") using FLAC compression level 8 (maximum compression)
 @echo off
 echo Messages about "skipping unknown chunk 'fact'" are normal.
-for /F "tokens=1 eol=:" %%a in (files.txt) do (
-  flac.exe -8 -f %%a
+pause
+
+for %%d in (metadata\*.dat) do (
+  :: convert WAVs (saved as "DAT") using FLAC's maximum compression
+  flac.exe -8 -s -f "%%~nxd"
+  :: copy metadata to the FLAC file
+  metaflac.exe "%%~nd.flac" --import-tags-from="%%d"
 )
 
-echo FLAC conversion (part 1 of 2) is done. You may want to scroll and verify that it worked. Messages about "skipping unknown chunk 'fact'" are normal.
-::Wait for user input
-pause
-::Patch using Scarab
-:: https://github.com/loyso/Scarab
-:: %CD% for the current directory
-patcher.exe --package "%CD%\RCT2_Music_Tags_Patch.zip" --old "%CD%" --stopIfError
-echo FLAC patching (part 2 of 2) is done. You may want to scroll up and verify that it worked.
-::Wait for user input
+echo FLAC conversion done. You may want to scroll and verify that it worked. Messages about "skipping unknown chunk 'fact'" are normal.
+
 pause
